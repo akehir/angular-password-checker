078bb21514768537103a95e47b61ef84997e014f
Raphael Ochsenbein <raphael@akehir.com>
2024-06-06 16:52:11 +0200

    Provide httpClient as well as httpClientTesting

--- projects/example-app-standalone/src/app/app.component.spec.ts
+++ projects/example-app-standalone/src/app/app.component.spec.ts
@@ -1,11 +1,12 @@
 import { TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {provideHttpClientTesting} from "@angular/common/http/testing";
+import { provideHttpClientTesting } from "@angular/common/http/testing";
+import { provideHttpClient } from "@angular/common/http";
 
 describe('AppComponent', () => {
   beforeEach(() => TestBed.configureTestingModule({
     imports: [AppComponent],
-    providers: [provideHttpClientTesting()]
+    providers: [provideHttpClient(), provideHttpClientTesting()]
   }));
 
   it('should create the app', () => {
